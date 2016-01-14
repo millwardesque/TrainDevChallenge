5cf76c9eaf6804553f108734efe08825273c9aca
Christopher Millward <christopher.millward@gmail.com>
2016-01-14 13:39:52 -0500

    Added unimplemented army spawner and partially implemented GUI editor for it.

---
+++ Assets/Editor/ArmySpawnerEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ArmySpawner))]
+public class ArmySpawnerEditor : Editor
+{
+	void OnSceneGUI () {
+		ArmySpawner myTarget = (ArmySpawner)target;
+		Color fillColour;
+		Color outlineColour;
+
+		if (myTarget.armyType == Army.RedArmy) {
+			fillColour = new Color(1f, 0.8f, 0.8f, 0.3f);
+			outlineColour = new Color(1f, 0f, 0f, 0.3f);
+		}
+		else {
+			fillColour = new Color(0.8f, 0.8f, 1f, 0.3f);
+			outlineColour = new Color(0f, 0f, 1f, 0.3f);
+		}
+
+		Handles.DrawSolidRectangleWithOutline(myTarget.spawnArea, fillColour, outlineColour);
+	}
+}
---
+++ Assets/Scripts/ArmySpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ArmySpawner : MonoBehaviour {
+	public Army armyType;
+	public Vector2 marchDirection;
+	public Soldier soldierPrefab;
+	public int maxSoldiers;
+	public float respawnTime;
+
+	public Rect spawnArea;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SpawnSoldier() {
+
+	}
+}
